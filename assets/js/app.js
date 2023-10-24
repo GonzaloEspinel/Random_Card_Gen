e221503af3d13efe27fc4047de84c635783adca0
Gonzalo Espinel <35546462+GonzaloEspinel@users.noreply.github.com>
2023-10-23 20:14:10 -0400

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 
 window.onload = function() {
   function randomPokerCard() {
-    const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-    const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+    const suits = ['♥', '♦', '♣', '	♠'];
+    const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'A'];
 
     const randomSuit = suits[Math.floor(Math.random() * suits.length)];
     const randomValue = values[Math.floor(Math.random() * values.length)];
