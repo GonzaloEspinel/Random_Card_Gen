6996f0a360f3155b99e99aa11fc6207a89037eb7
Gonzalo Espinel <gonzaloespinel@outlook.com>
2023-10-25 14:12:20 -0400

    changes5

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,33 @@
 
 window.onload = function() {
-  function randomPokerCard() {
-    const suits = ['♥', '♦', '♣', '	♠'];
-    const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'A'];
 
+  //  This function generates a random suit and value 
+  function randomPokerCard() {
+    const suits = ['♥', '♦', '♣', '♠'];
+    const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+// Math.random() returns a random number
+// Math.floor() is used to get this value
     const randomSuit = suits[Math.floor(Math.random() * suits.length)];
     const randomValue = values[Math.floor(Math.random() * values.length)];
 
-    return `${randomValue} of ${randomSuit}`;
-}
+    // querySelector = selects css class , innerHTML sends result to html div class 
 
-console.log(randomPokerCard());
+    document.querySelector(".topSuit").innerHTML = randomSuit;
+    document.querySelector(".cardNum").innerHTML = randomValue;
+    document.querySelector(".botSuit").innerHTML = randomSuit;
 
+    // querySelector = selects css class assigns color by comparison of result 
+
+    if (randomSuit === "♥" || randomSuit === "♦") {
+      document.querySelector(".card").style.color = "crimson";
+    } else {
+      document.querySelector(".card").style.color = "black";
+    }
+    // get new card button generates new card
+    // Object.onclick = randomPokerCard(document.getElementsByClass(".card").innerHTML = "The new card is: " + randomValue + " of " + randomSuit);
+    
+  }
+
+  // Call the function when the window loads
+  randomPokerCard();
 }
